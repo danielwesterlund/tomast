ca9e4384cbe02e12a64f94449d1a5e5581b72195
Daniel Westerlund <75675328+danielwesterlund@users.noreply.github.com>
2023-12-11 15:39:17 +0100

    Hello World

--- mainfile.py
+++ mainfile.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def main():
-    st.title("Hello, Streamlit!")
+    st.title("This is Tomast")
     st.write("This is a simple Streamlit app.")
 
 if __name__ == "__main__":
