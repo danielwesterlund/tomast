88bafef9ff88c6cc14c0c7f68f5a0c2dbd00cf0c
Daniel Westerlund <75675328+danielwesterlund@users.noreply.github.com>
2023-12-11 16:10:53 +0100

    file strcuture and first try

---
+++ index.py
@@ -0,0 +1,33 @@
+import os
+import openai
+import streamlit as st
+
+# ------- General UI -------
+
+def homeui():
+    st.title("This is Tomast")
+
+    st.markdown("""
+    Welcome to Tomast! This is TOMAST, an AI-powered operating system to facilitate the creation of scripts and materials for military exercises and training. 
+    Let's get started!
+    """)
+
+    st.write("Please define the Scenario")
+    st.markdown("")
+    st.subheader('Please define the Scenario 👇')
+
+    # Check if 'Scenario' already exists in session_state
+    # If not, then initialize it
+    if 'scenario' not in st.session_state:
+        st.session_state['scenario'] = "The war in Kosovo"
+
+    # If it is, update it
+    st.session_state['scenario'] = st.text_area("Scenario", value=st.session_state.get('scenario', ''), placeholder="Enter your scenario here and continue by clicking on ...")
+
+    st.success("Understood! The Scenario has been updated")
+
+def main():
+    homeui()
+
+if __name__ == "__main__":
+    main()
