efd98fa498e9f938176385fe34dc971b2ddd7da0
Daniel Westerlund <75675328+danielwesterlund@users.noreply.github.com>
2023-12-11 16:49:02 +0100

    .

--- mainfile.py
+++ mainfile.py
@@ -1,11 +1,12 @@
-import openai
+import os
+#import openai
 import streamlit as st
 
 
 # ------- Constants and Configuration --------
 
-os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
-openai.api_key = os.environ['OPENAI_API_KEY']
+#os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+#openai.api_key = os.environ['OPENAI_API_KEY']
 st.set_page_config(page_title="Tomast", page_icon="🪖", layout='wide')
 
 
