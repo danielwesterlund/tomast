668da29aee47792ca048cc098add6446e775b00a
Daniel Westerlund <75675328+danielwesterlund@users.noreply.github.com>
2023-12-11 16:16:27 +0100

    Matrix hello world

---
+++ pages/matrix.py
@@ -0,0 +1,16 @@
+import os
+import openai
+import streamlit as st
+
+# ------- General UI -------
+
+def homeui():
+    st.title("This is Matrix")
+
+
+
+def main():
+    homeui()
+
+if __name__ == "__main__":
+    main()
