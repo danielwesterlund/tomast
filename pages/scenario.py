268db05a9998ff21842330ef81d3421f51e6a373
Daniel Westerlund <75675328+danielwesterlund@users.noreply.github.com>
2023-12-13 15:27:47 +0100

    .

--- pages/scenario.py
+++ pages/scenario.py
@@ -1,18 +1,24 @@
 import os
-#import openai
 import streamlit as st
 
 
 def homeui():
     st.title("This is Scenario")
-    st.write(st.session_state['scenario'])
+    if 'scenario' in st.session_state:
+        st.write(st.session_state['scenario'])
+    else:
+        st.write("No scenario selected.")
 
     st.subheader("Scenario Summary")
-    st.text_area("Summary", "Enter the summary here...", height=200)
+    summary = st.text_area("Summary", "Enter the summary here...", height=200)
+    if summary:
+        st.session_state['summary'] = summary
+
     st.subheader("Scenario Images")
     uploaded_file = st.file_uploader("Choose an image...", type="jpg")
     if uploaded_file is not None:
         st.image(uploaded_file)
+
     st.subheader("Scenario Maps")
     uploaded_map = st.file_uploader("Choose a map...", type="jpg")
     if uploaded_map is not None:
