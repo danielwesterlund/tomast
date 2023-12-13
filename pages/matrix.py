aacbb5b2ac14c0c2b96004637dadb21017e0cef3
Daniel Westerlund <75675328+danielwesterlund@users.noreply.github.com>
2023-12-13 15:31:58 +0100

    .

--- pages/matrix.py
+++ pages/matrix.py
@@ -13,7 +13,7 @@ def homeui():
     st.title("This is The Relationship Matrix")
     
     # Connect to Neo4j Database
-    graph = Graph("bolt://localhost:7687", auth=("neo4j", "password"))
+    #graph = Graph("bolt://localhost:7687", auth=("neo4j", "password"))
 
     # Input box for CYPHER query
     query = st.text_input('Enter your CYPHER query here', 'MATCH (n) RETURN n')
@@ -23,19 +23,19 @@ def homeui():
         query = 'MATCH (n) RETURN n'
 
     # Use the Streamlit Component to display the Neo4j Graph
-    neo4j_component(graph.run(query).data())
+    #neo4j_component(graph.run(query).data())
 
     # Input box for user prompt to change the matrix
     prompt = st.text_input('Enter your prompt to change the matrix')
     if st.button('Submit'):
         # Show the query as CYPHER and update the graph
         st.write('Your CYPHER query: ', prompt)
-        neo4j_component(graph.run(prompt).data())
+        #neo4j_component(graph.run(prompt).data())
 
 
         # Confirm changes and update the graph on the Neo4j server
         if st.button('Confirm'):
-            graph.run(prompt)
+            #graph.run(prompt)
             st.success('Graph updated successfully!')
 
 def main():
