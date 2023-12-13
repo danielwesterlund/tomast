ce1f22599af85571c5e6df402882a79a5d85d21b
Daniel Westerlund <75675328+danielwesterlund@users.noreply.github.com>
2023-12-13 15:21:58 +0100

    .

--- pages/roles.py
+++ pages/roles.py
@@ -15,15 +15,23 @@ def homeui():
     people = ["Person 1", "Person 2", "Person 3"]  # Replace with actual names
     selected_person = st.sidebar.selectbox("Select a person", people)
     
-    # Display details for selected person
+    # Display details for selected person in a table
     if selected_person:
         st.subheader(f"Details for {selected_person}")
-        st.text("Name: ")  # Replace with actual data
-        st.text("Age: ")  # Replace with actual data
-        st.text("Date of Birth: ")  # Replace with actual data
-        st.text("Educational History: ")  # Replace with actual data
-        st.text("Biography: ")  # Replace with actual data
-        st.text("Character Traits: ")  # Replace with actual data
-        st.text("Ethnicity: ")  # Replace with actual data
-        st.text("Personality: ")  # Replace with actual data
-        st.text("Motivations: ")  # Replace with actual data
+        details = {
+            "Name": "",  # Replace with actual data
+            "Age": "",  # Replace with actual data
+            "Date of Birth": "",  # Replace with actual data
+            "Educational History": "",  # Replace with actual data
+            "Biography": "",  # Replace with actual data
+            "Character Traits": "",  # Replace with actual data
+            "Ethnicity": "",  # Replace with actual data
+            "Personality": "",  # Replace with actual data
+            "Motivations": "",  # Replace with actual data
+        }
+        st.table(details)
+def main():
+    homeui()
+
+if __name__ == "__main__":
+    main()
--- pages/scenario.py
+++ pages/scenario.py
@@ -4,9 +4,19 @@ import streamlit as st
 
 
 def homeui():
-    st.title("This is Matrix")
+    st.title("This is Scenario")
     st.write(st.session_state['scenario'])
 
+    st.subheader("Scenario Summary")
+    st.text_area("Summary", "Enter the summary here...", height=200)
+    st.subheader("Scenario Images")
+    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+    if uploaded_file is not None:
+        st.image(uploaded_file)
+    st.subheader("Scenario Maps")
+    uploaded_map = st.file_uploader("Choose a map...", type="jpg")
+    if uploaded_map is not None:
+        st.image(uploaded_map)
 
  
 def main():
