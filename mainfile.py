6db8b3ea7d3e58c3aed96e121fe9099aa1450b38
Daniel Westerlund <75675328+danielwesterlund@users.noreply.github.com>
2023-12-18 15:46:03 +0100

    scenario summary

--- mainfile.py
+++ mainfile.py
@@ -14,10 +14,33 @@ st.session_state['model'] = "gpt-3.5-turbo"
 st.session_state['language'] = "German"
 
 
+# ------- OpenAI Call -------
+
+def call_openai_api(prompt, max_tokens=1000, temperature=0.4):
+    model = st.session_state['model']
+    language = st.session_state['language']
+    system = f"""You always generate in {language}."""
+    try:
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=[
+                {"role": "system", "content": system},
+                {"role": "user", "content": prompt}
+            ],
+            max_tokens=max_tokens,
+            temperature=temperature
+        )
+        return response['choices'][0]['message']['content'].strip()
+    except (KeyError, Exception) as e:
+        return f"An error occurred: {e}"
+    
+
+
 # ------- General UI -------
 
 def homeui():
     st.title("This is Tomast")
+
 
     st.markdown("""
     Welcome to Tomast! This is TOMAST, an AI-powered operating system to facilitate the creation of scripts and materials for military exercises and training. 
@@ -63,6 +86,29 @@ def call_openai_api(prompt, max_tokens=1000, temperature=0.4):
     except (KeyError, Exception) as e:
         return f"An error occurred: {e}"
     
+
+
+    # ------- Scenario Summary  -------#
+
+def scenario_summary():
+    prompt = f"""The first input you always need is the Scenario which serves as the fundamental context in which the scenario takes place. This could either be a functional scenario which will include places, organisations and history. It will usually have a military context and include conflict and potential for conflict.
+
+You will analyse this Scenario in detail:  {st.session_state['scenario']}
+
+and will extract:
+key people, 
+alliances, 
+important events, 
+conflicting parties, 
+potential betrayals 
+and potentials for peace.
+
+You will then summarise the scenario in 2-3 paragraphs.
+    """
+    scenario_summary = call_openai_api(prompt, max_tokens=2000)
+    st.session_state['scenario'] = scenario_summary
+    st.success("Summary generated...")
+    return scenario_summary
 
 
 def main():
