957140a9bebb11847101914b01a560095927d617
Daniel Westerlund <75675328+danielwesterlund@users.noreply.github.com>
2023-12-18 15:59:13 +0100

    .

--- mainfile.py
+++ mainfile.py
@@ -7,16 +7,17 @@ from openai import OpenAI
 
 # ------- Constants and Configuration --------
 
-os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
-openai.api_key = os.environ['OPENAI_API_KEY']
-
-# Create an instance of the OpenAI API client
-client = openai.ApiClient()
-
 st.set_page_config(page_title="Tomast", page_icon="🪖", layout='wide')
 
 st.session_state['model'] = "gpt-3.5-turbo"
 st.session_state['language'] = "German"
+
+
+# Create an instance of the OpenAI API client
+client = openai.ApiClient(
+os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+openai.api_key = os.environ['OPENAI_API_KEY']
+)
 
 
 # ------- OpenAI Call -------
