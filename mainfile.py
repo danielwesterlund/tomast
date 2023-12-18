3370d6f714004a33ec42fcb0f218aa77eed0402e
Daniel Westerlund <75675328+danielwesterlund@users.noreply.github.com>
2023-12-18 17:33:57 +0100

    .

--- mainfile.py
+++ mainfile.py
@@ -63,7 +63,7 @@ all important places,
 You will also analyse how these are connected and arranging them into a graph database where you will define the relationships between the nodes.
 
     """
-    scenario_summary = call_openai_api(prompt, max_tokens=2000)
+    scenario_summary = call_openai_api(prompt, max_tokens=1000)
     st.session_state['scenario'] = scenario_summary
     st.success("Summary generated...")
     return scenario_summary
